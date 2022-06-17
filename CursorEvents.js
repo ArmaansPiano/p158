dd2fc76c72b4f99972c52e8bfdd5bd84b95e8583
ArmaansPiano <71108324+ArmaansPiano@users.noreply.github.com>
2022-06-16 21:01:33 -0500

    Add files via upload

---
+++ CursorEvents.js
@@ -0,0 +1,48 @@
+AFRAME.registerComponent("cursor-listener", {
+    schema: {
+      selectedItemId: { default: "", type: "string" },
+    },
+    init: function () {
+      this.handleMouseEnterEvents();
+      this.handleMouseLeaveEvents();
+    },
+  
+    handlePlacesListState: function () {
+      const id = this.el.getAttribute("id");
+      const placesId = ["captain-aero", "outer-space", "spiderman", "superman"];
+      if (placesId.includes(id)) {
+        const placeContainer = document.querySelector("#places-container");
+        placeContainer.setAttribute("cursor-listener", {
+          selectedItemId: id,
+        });
+        this.el.setAttribute("material", {
+          color: "orange",
+          opacity: 1,
+        });
+      }
+    },
+    handleMouseEnterEvents: function () {
+      // Mouse Enter Events
+      this.el.addEventListener("mouseenter", () => {
+        this.handlePlacesListState();
+      });
+    },
+    handleMouseLeaveEvents: function () {
+      // Mouse Leave Events
+      this.el.addEventListener("mouseleave", () => {
+        const {selectedItemId} = this.data;
+        if(selectedItemId){
+          const el = document.querySelector(`#${selectedItemId}`);
+          const id = el.getAttribute("id");
+          if(id == selectedItemId) {
+            el.setAttribute("material",{
+              color: "#0077CC",
+              opacity: 1,
+            });
+          }
+        }
+      });
+      
+    },
+  });
+  
